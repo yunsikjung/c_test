b98b711254884df11fdeb8e15703b32477949399
yunsik <yunsikjung@mines.edu>
2019-01-16 22:28:18 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Academy";
+    int age = 50;
+    double gpa = 4.0;
+    cout << grade << endl;
+    return 0;
+}
