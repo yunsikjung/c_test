809d62f43a815a15a91bbba76cea9f3eed687049
yunsik <yunsikjung@mines.edu>
2019-01-16 22:48:04 -0700

    plus 2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main()
 {
     char grade = 'A';
-    string phrase = "Academy";
+    string phrase = "Academy2";
     int age = 50;
     double gpa = 4.0;
     cout << grade << endl;
     return 0;
+
 }
